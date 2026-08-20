a4ee337d4ceedafb05d858b6b50dbe42b426a66f
Riccardo Brotto <ext-brotto_r@psi.ch>
2026-08-20 19:55:06 +0200

    adding the Config directories and the COnfig files containing Config_function -> initialices the data struct

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,1 +1,67 @@
-// funzioni da inserire per la lettura del file json
+#include "Config.h"
+#include <nlohmann/json.hpp>
+
+#include <fstream>
+#include <iostream>
+
+int Configure(config_struct& value, std::string json_path) {
+    std::ifstream file(json_path);
+    
+    if (!file.is_open()) {
+        std::cout << "ERROR: Cannot open: " << json_path << ", exiting.\n" << std::endl;
+        return 1;
+    }
+
+    try {
+        nlohmann::json j;
+        file >> j;
+
+        if (j.contains("io")) {
+            if (j["io"].contains("tree_name")) {
+                value.io.tree_name = j["io"]["tree_name"];
+            }
+            if (j["io"].contains("input_file")) {
+                value.io.input_file = j["io"]["input_file"];
+            }
+        }
+
+        if (j.contains("flag")) {
+            if (j["flag"].contains("en_kinematics")) {
+                value.flag.en_kinematics = j["flag"]["en_kinematics"];
+            }
+            if (j["flag"].contains("en_isolation")) {
+                value.flag.en_isolation = j["flag"]["en_isolation"];
+            }
+            if (j["flag"].contains("en_opposite_charge")) {
+                value.flag.en_opposite_charge = j["flag"]["en_opposite_charge"];
+            }
+            if (j["flag"].contains("en_mass_window")) {
+                value.flag.en_mass_window = j["flag"]["en_mass_window"];
+            }
+        }
+
+        if (j.contains("cut")) {
+            if (j["cut"].contains("pt_cut")) {
+                value.cut.pt_min = j["cut"]["pt_min"];
+            }
+            if (j["cut"].contains("eta_cut")) {
+                value.cut.eta_max = j["cut"]["eta_max"];
+            }
+            if (j["cut"].contains("iso_cut")) {
+                value.cut.iso_max = j["cut"]["iso_max"];
+            }
+            if (j["cut"].contains("mass_min")) {
+                value.cut.mass_min = j["cut"]["mass_min"];
+            }
+            if (j["cut"].contains("mass_max")) {
+                value.cut.mass_max = j["cut"]["mass_max"];
+            }
+        }
+    } catch (const std::exception& except) {
+        std::cout << "ERROR: " << except.what() << std::endl;
+    }
+
+    return 0;
+}
+
+void Verbose_config() {}
