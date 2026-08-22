7e9f0eb0dc8050517f8fc7938be2b06c81e3353f
Riccardo Brotto <ext-brotto_r@psi.ch>
2026-08-22 14:49:03 +0200

    documenting mass_leptons + phi_star functions

--- include/Utils.h
+++ include/Utils.h
@@ -4,6 +4,15 @@
 #include <ROOT/RVec.hxx>
 #include <cmath>
 
+/**
+ * @brief Calculate the invariant mass of the first two particles in the event.
+ * @tparam Template: float, double.
+ * @param pt Is the RVec containing the transverse momentum of the particles.
+ * @param eta Is the pseudorapidity of the particles.
+ * @param phi Is the angular variable in cilindrical cordinates.
+ * @param mass Mass values of the particles.
+ * @return Returns the invariant mass depending on the input type.
+ */
 template <typename T>
 T mass_leptons(const ROOT::RVec<T>& pt, const ROOT::RVec<T>& eta, const ROOT::RVec<T>& phi, const ROOT::RVec<T>& mass) {
     
@@ -12,6 +21,13 @@ T mass_leptons(const ROOT::RVec<T>& pt, const ROOT::RVec<T>& eta, const ROOT::RV
 
 }
 
+/**
+ * @brief Calculate the special angular variable between the first two particles in the event.
+ * @tparam Template: float, double.
+ * @param eta Is the pseudorapidity of the particles.
+ * @param phi Is the angular variable in cilindrical cordinates.
+ * @return Returns special angular variable depending on the input type.
+ */
 template <typename T>
 T phi_star(const ROOT::RVec<T>& eta, const ROOT::RVec<T>& phi) {
     T delta_phi = std::abs(ROOT::VecOps::DeltaPhi(phi[0], phi[1]));
