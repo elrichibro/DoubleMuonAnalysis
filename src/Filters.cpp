10cba17621452a036f5c6fba387d3575459e65b6
Riccardo Brotto <ext-brotto_r@psi.ch>
2026-08-26 18:22:02 +0200

    Adding Filters MonteCarlo documentation

--- src/Filters.cpp
+++ src/Filters.cpp
@@ -48,6 +48,30 @@ ROOT::RVec<bool> GoodMuon_filter::operator()(const ROOT::RVec<float>& pt, const 
     return mask;
 }
 
+
+/* Flags summary:
+    0 : isPrompt                -> SELECTED
+    1 : isDecayedLeptonHadron
+    2 : isTauDecayProduct
+    3 : isPromptTauDecayProduct
+    4 : isDirectTauDecayProduct
+    5 : isDirectPromptTauDecayProduct
+    6 : isDirectHadronDecayProduct
+    7 : isHardProcess
+    8 : fromHardProcess         -> SELECTED
+    9 : isHardProcessTauDecayProduct
+    10 : isDirectHardProcessTauDecayProduct
+    11 : fromHardProcessBeforeFSR
+    12 : isFirstCopy
+    13 : isLastCopy             -> SELECTED
+    14 : isLastCopyBeforeFSR
+
+
+    Bitwie mask: 0, 8, 13 -> 2^13 + 2^8 + 2^0 = 8192 + 256 + 1 = 8449           
+    0 x ( 0 0 1 0 )( 0 0 0 1 )( 0 0 0 0 )( 0 0 0 1 ) = 0x2101
+
+*/
+
 ROOT::RVec<bool> is_MC_Z0(const ROOT::RVec<Int_t>& pdgId, const ROOT::RVec<Int_t>& flags) {
     return ((pdgId == 23) && ((flags & 0x2101) == 0x2101));
 }
