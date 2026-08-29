394b5a2b98f9838be273cc455a8cad8faccc6ab7
Riccardo Brotto <ext-brotto_r@psi.ch>
2026-08-29 11:24:41 +0200

    Adding comments to the functions -> better understanding

--- src/Filters.cpp
+++ src/Filters.cpp
@@ -1,22 +1,31 @@
 #include "Filters.h"
 #include "Utils.h"
 
+// #################################################################
+
 bool Validation_filter::operator()(const UInt_t run, const UInt_t lum_block) const {       
+    // Auto-update variables -> applied to multithread operation mode.
     thread_local static UInt_t last_run = 0;
     thread_local static UInt_t last_lum_block = 0;
-    thread_local static bool last_decision = false;
+    thread_local static bool last_decision = false;// For fast loop
 
+    // Fast loop
     if ((run == last_run) && (lum_block == last_lum_block)) {
         return last_decision;
     }
 
+    // Updating values
     last_run = run;
     last_lum_block = lum_block;
 
+    // Checks the run input within the validation_map
     auto it = val_map.find(run);
 
+    // if succeds
     if (it != val_map.end()) {
+        // Checking blocks
         for (const auto& block : it->second) {
+            // Luminosity block within the range
             if ((lum_block >= block.first) && (lum_block <= block.second)) {
                 last_decision = true;
                 return true;
@@ -24,10 +33,13 @@ bool Validation_filter::operator()(const UInt_t run, const UInt_t lum_block) con
         }
     }
 
+    // else option 
     last_decision = false;
     
     return false;
 }
+
+// #################################################################
 
 ROOT::RVec<bool> GoodMuon_filter::operator()(const ROOT::RVec<float>& pt, const ROOT::RVec<float>& eta, const ROOT::RVec<bool>& tight_id) const {
     
@@ -44,43 +56,60 @@ ROOT::RVec<bool> GoodMuon_filter::operator()(const ROOT::RVec<float>& pt, const 
     return mask;
 }
 
+// #################################################################
+
 ROOT::RDF::RNode node_Kin_cut(const std::string mask_name, ROOT::RDF::RNode node, const std::string pt, 
     const std::string eta, float pt_cut, float eta_cut) {
     
     ROOT::RDF::RNode node_kin_cut = node
     .Define(mask_name, [pt_cut, eta_cut](const ROOT::RVec<float>& pt, const ROOT::RVec<float>& eta) {
+        
+        // Physical cut
         return ((pt > pt_cut) && (abs(eta) < eta_cut));
+
     }, {pt, eta});
 
     return node_kin_cut;
 }
 
+// #################################################################
+
 ResultsTagAndProbe TagAndProbe(const MuonKinematics& kin, const MuonFlags& flags) {
+    // Results container
     ResultsTagAndProbe results;
     
+    // Number of particles in the event
     const unsigned int n_muons = kin.pt.size();
     
     for(int i = 0; i < n_muons; i++) {
-        
+        // Fast exit
         if (!flags.tag[i]) {
             continue;
+        
+        // Good condition
         } else if (flags.tag[i]) {
 
             for (int j = 0; j < n_muons; j++) {
+                // Fast exit
                 if (i == j || !flags.probe[j]) {
                     continue;
+
+                // Loop into good probe muons
                 } else if ((j != i) && flags.probe[j]) {
+                    // Invariant mass (tag + probe)
                     float mass = ROOT::VecOps::InvariantMass(ROOT::RVec<float>{kin.pt[i], kin.pt[j]}, 
                         ROOT::RVec<float>{kin.eta[i], kin.eta[j]}, ROOT::RVec<float>{kin.phi[i], kin.phi[j]},
                         ROOT::RVec<float>{kin.mass[i], kin.mass[j]});
                     
+                    // Invariant mass range
                     if((mass > 60) && (mass < 120)) {
-                        // probe all
+                        
+                        // All probes (passed + failed)
                         results.pt_all.push_back(kin.pt[j]);
                         results.eta_all.push_back(kin.eta[j]);
 
                         if ((flags.global[j]) && (flags.iso[j] < 0.15)) {
-                            // probe all
+                            // Passed probes
                             results.pt_pass.push_back(kin.pt[j]);
                             results.eta_pass.push_back(kin.eta[j]);
                         } else {
@@ -93,7 +122,6 @@ ResultsTagAndProbe TagAndProbe(const MuonKinematics& kin, const MuonFlags& flags
             }
         }
     }
+    
     return results;
 }
-
-
