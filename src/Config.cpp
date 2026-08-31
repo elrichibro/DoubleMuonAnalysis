f6659a961f794791cfb1fb88a24911fe928a883f
Riccardo Brotto <ext-brotto_r@psi.ch>
2026-08-31 17:04:37 +0200

    Adding verbose config options

--- src/Config.cpp
+++ src/Config.cpp
@@ -124,14 +124,6 @@ void Verbose_config(const config_struct& value) {
     std::cout << "Init Verbose -> Loaded Configuration:" << std::endl;
     
     std::cout << "" << std::endl;
-    
-    std::cout << "Input data file: " << value.io.in_data_file << std::endl;
-    std::cout << "Data Tree: " << value.io.tree_data_name << std::endl;
-    std::cout << "Input MC file: " << value.io.in_mc_file << std::endl;
-    std::cout << "MC Tree: " << value.io.tree_mc_name << std::endl;
-    
-    
-    std::cout << "" << std::endl;
 
     std::cout << "General settings:" << std::endl;
     std::cout << "    Dataset used: " << value.general.dataset << std::endl;
@@ -141,8 +133,8 @@ void Verbose_config(const config_struct& value) {
     std::cout << "    Save flag: " << value.general.save << std::endl;
     
     std::cout << "" << std::endl;
+    
     std::cout << "I/O settings:" << std::endl;
-
     std::cout << "    Input data file: " << value.io.in_data_file << std::endl;
     std::cout << "    Data Tree: " << value.io.tree_data_name << std::endl;
     std::cout << "    Input MC file: " << value.io.in_mc_file << std::endl;
@@ -221,79 +213,6 @@ void Verbose_config(const config_struct& value) {
     std::cout << "        Width: " << value.canvas.width << std::endl;
     std::cout << "        Height: " << value.canvas.height << std::endl;
   
-    std::cout << "" << std::endl;
-
-    std::cout << "Validation file: " << value.io.val_file << std::endl;
-    std::cout << "Output file: " << value.io.output_file << std::endl;
-
-    std::cout << "" << std::endl;
-
-    std::cout << "TagAndProbe flags:" << std::endl;
-    std::cout << "    Kinematics: " << value.flag_TP.en_kinematics << std::endl;
-    std::cout << "    Isolation: " << value.flag_TP.en_isolation << std::endl;
-    std::cout << "    Mass window: " << value.flag_TP.en_mass_window << std::endl;
-    std::cout << "    Tight muon: " << value.flag_TP.en_tight_muon << std::endl;
-
-    std::cout << "" << std::endl;
-
-    std::cout << "TagAndProbe cuts:" << std::endl;
-    std::cout << "    p_T cut: " << value.cut_TP.pt_cut << std::endl;
-    std::cout << "    Eta cut: " << value.cut_TP.eta_cut << std::endl;
-    std::cout << "    Isolation cut: " << value.cut_TP.iso_cut << std::endl;
-    std::cout << "    Max mass: " << value.cut_TP.mass_max << std::endl;
-    std::cout << "    Min mass: " << value.cut_TP.mass_min << std::endl;
-
-    std::cout << "" << std::endl;
-
-    std::cout << "RespMatrix flags:" << std::endl;
-    std::cout << "    Kinematics: " << value.flag_RM.en_kinematics << std::endl;
-    std::cout << "    Isolation: " << value.flag_RM.en_isolation << std::endl;
-    std::cout << "    Mass window: " << value.flag_RM.en_mass_window << std::endl;
-    std::cout << "    Tight muon: " << value.flag_RM.en_tight_muon << std::endl;
-
-    std::cout << "" << std::endl;
-
-    std::cout << "RespMatrix cuts:" << std::endl;
-    std::cout << "    p_T cut: " << value.cut_RM.pt_cut << std::endl;
-    std::cout << "    Eta cut: " << value.cut_RM.eta_cut << std::endl;
-    std::cout << "    Isolation cut: " << value.cut_RM.iso_cut << std::endl;
-    std::cout << "    Max mass: " << value.cut_RM.mass_max << std::endl;
-    std::cout << "    Min mass: " << value.cut_RM.mass_min << std::endl;
-
-    std::cout << "" << std::endl;
-
-    std::cout << "Plot settings:" << std::endl;
-
-    std::cout << "" << std::endl;
-
-    std::cout << "    Pt plot: " << std::endl;
-    std::cout << "        Axis title: " << value.pt_plot.title_axis << std::endl;
-    std::cout << "        Axis min: " << value.pt_plot.axis_min << std::endl;
-    std::cout << "        Axis max: " << value.pt_plot.axis_max << std::endl;
-    std::cout << "        Number of bins: " << value.pt_plot.nbins << std::endl;
-
-    std::cout << "" << std::endl;
-
-    std::cout << "    Eta plot: " << std::endl;
-    std::cout << "        Axis title: " << value.eta_plot.title_axis << std::endl;
-    std::cout << "        Axis min: " << value.eta_plot.axis_min << std::endl;
-    std::cout << "        Axis max: " << value.eta_plot.axis_max << std::endl;
-    std::cout << "        Number of bins: " << value.eta_plot.nbins << std::endl;
-
-    std::cout << "" << std::endl;
-
-    std::cout << "    Invariant mass plot: " << std::endl;
-    std::cout << "        Axis title: " << value.mll_plot.title_axis << std::endl;
-    std::cout << "        Axis min: " << value.mll_plot.axis_min << std::endl;
-    std::cout << "        Axis max: " << value.mll_plot.axis_max << std::endl;
-    std::cout << "        Number of bins: " << value.mll_plot.nbins << std::endl;
-
-    std::cout << "" << std::endl;
-
-    std::cout << "    Canvas settings: " << std::endl;
-    std::cout << "        Width: " << value.canvas.width << std::endl;
-    std::cout << "        Height: " << value.canvas.height << std::endl;
-
     std::cout << "" << std::endl;
 
     std::cout << "End of Verbose" << std::endl;
